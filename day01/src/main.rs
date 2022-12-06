097a40ecbc031123441e8e7d0a4243cbc5055ad2
S1monG <si5564gu-s@student.lu.se>
2022-12-06 21:35:12 +0100

    Initial commit and day01 done.

---
+++ day01/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs::read_to_string;
+
+fn main() {
+
+    let input = read_to_string("input/input.txt").unwrap();
+
+    let sp = input.split("\r\n\r\n");
+    let totals = sp.map::<usize, _>(|s| 
+        s.split("\r\n").map(|number| 
+            number.parse::<usize>().unwrap_or(0)
+        ).sum()
+    );
+
+    println!("{}", totals.clone().max().unwrap());
+
+    let mut top_three = vec![0,0,0];
+
+    for nbr in totals {
+        if nbr > *top_three.iter().min().unwrap() {
+            top_three.sort();
+            top_three.remove(0);
+            top_three.push(nbr);
+        }
+    }
+
+    println!("{}", top_three.iter().sum::<usize>());
+}
+
+
