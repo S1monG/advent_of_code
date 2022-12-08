301a2d172f92621f3dc569abe24bc5450dad12e6
S1monG <si5564gu-s@student.lu.se>
2022-12-08 22:00:16 +0100

    Day 3 completed.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,11 +3,6 @@ use std::collections::*;
 
 fn main() {
 
-    part1();
-}
-
-fn part1() {
-
     let input = read_to_string("input/input.txt");
 
     let mut h: HashMap<char, usize> = HashMap::new();
@@ -23,6 +18,7 @@ fn part1() {
     
     let mut sum = 0;
 
+    // PART 1
     for line in input.unwrap().lines() {
         let half = line.len()/2;
         let first_half: Vec<char> = line.chars().take(half).collect();
@@ -35,5 +31,30 @@ fn part1() {
         }
     }
 
-    println!("{}", sum);
+    println!("Part 1: {}", sum);
+
+    // PART 2
+    let input = read_to_string("input/input.txt").unwrap();
+    let vec: Vec<&str> = input.lines().collect();
+
+    let mut sum = 0;
+
+    for i in (0..vec.len()).step_by(3) {
+        // There doesn't seem to exist an easy way to get the intersection over multiple sets
+        // I have to do it in two steps.
+        let set1: HashSet<char> = HashSet::from_iter(vec.get(i).unwrap().chars());
+        let set2: HashSet<char> = HashSet::from_iter(vec.get(i+1).unwrap().chars());
+        let set3: HashSet<char> = HashSet::from_iter(vec.get(i+2).unwrap().chars());
+        let xs: HashSet<char> = set1.intersection(&set2).cloned().collect();
+        let ys: HashSet<char> = set3.intersection(&xs).cloned().collect();
+
+        assert!(ys.len() == 1);
+        let c = ys.iter().next().unwrap();
+        sum += h.get(c).unwrap();
+
+    }
+
+    println!("Part 2: {}", sum);
+
 }
+
