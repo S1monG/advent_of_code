c64b5026757432ec6310e9aab80d8830048c13b2
S1monG <si5564gu-s@student.lu.se>
2022-12-10 18:43:04 +0100

    Working on part 2.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -19,6 +19,14 @@ fn main() {
 
     let mut stack_part2 = stack_part1.clone();
 
+    for stack in stack_part1.iter() {
+        println!("{:?}  ", stack);
+    }
+    println!();
+    for stack in stack_part2.iter() {
+        println!("{:?}  ", stack);
+    }
+
     for line in input_moves.lines() {
         let moves: Vec<u8> = line.split(" ").filter_map(|s| s.parse::<u8>().ok()).collect();
         let (iterations, from_stack, to_stack) = (*moves.get(0).unwrap(),
@@ -26,26 +34,42 @@ fn main() {
                                                                  *moves.get(2).unwrap()
         );
 
-        let index = stack_part2[usize::from(to_stack-1)].len();
-
         for _ in 0..iterations {
-
-            // Part 1
             let crate_to_be_moved = stack_part1[usize::from(from_stack-1)].pop().unwrap();
             stack_part1[usize::from(to_stack-1)].push(crate_to_be_moved);
-
-            stack_part2[usize::from(to_stack-1)].insert(index, crate_to_be_moved);
         }
-
     }
 
-    println!("Part 1:");
+    /* println!("Part 1:");
     for stack in stack_part1.iter() {
         print!("{:?}  ", stack.last().unwrap());
+    } */
+
+    //part2(stack_part2);
+
+}
+
+fn part2(mut stacks: [Vec<char>; 9]) -> Option<()> {
+
+    let input_moves = read_to_string("input/input.txt").unwrap();
+
+    for line in input_moves.lines() {
+        let mut sp = line.split(" ");
+        let count = sp.nth(1)?.parse::<usize>().unwrap();
+        let from_stack = sp.nth(1)?.parse::<usize>().unwrap();
+        let to_stack = sp.nth(1)?.parse::<usize>().unwrap();
+        println!("Length of stack is {}    subtract with {}", stacks[from_stack].len(), count);
+        let split_at = stacks[from_stack].len() - count;
+
+        let mut crates = stacks[from_stack].split_off(split_at);
+        stacks[to_stack].append(&mut crates);
+
     }
-    println!("\n\nPart 2:");
-    for stack in stack_part2.iter() {
+
+    println!("Part 2:");
+    for stack in stacks.iter() {
         print!("{:?}  ", stack.last().unwrap());
     }
 
+    Some(())
 }
