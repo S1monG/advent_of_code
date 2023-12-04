15f33bb9d150a752ff5bf1a7c68c30d56369569d
S1monG <si5564gu-s@student.lu.se>
2023-12-04 19:29:19 +0100

    222222222

--- 2023/day02/src/main.py
+++ 2023/day02/src/main.py
@@ -17,16 +17,33 @@ def solve(input):
                 return False
     return True
             
+def solve2(input):
+    a = input.split(':')[1].split(';') # removes game index
+    d = {"red": 0, "green": 0, "blue": 0}
+    for nbr_string_pairs in a:
+        l = nbr_string_pairs.split(',')
 
+        for pair in l:
+            nbr = int(pair.split()[0])
+            color = pair.split()[1].strip()
+            if d[color] < nbr:
+                d[color] = nbr
+    res = 1
+    for v in d.values():
+        res*=v
+    return res
 
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 rel_path = "input.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 sum1 = 0
+sum2 = 0
 with open(abs_file_path) as file:
     for idx, line in enumerate(file):
         if solve(line):
             sum1 += idx + 1
+        sum2 += solve2(line)
 print(sum1)
+print(sum2)
 
