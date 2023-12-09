ccb4af934dabf8bd3b425edb955c5eb6db1b7d13
S1monG <si5564gu-s@student.lu.se>
2023-12-09 15:50:10 +0100

    Optimize solution

--- 2023/day09/src/main.py
+++ 2023/day09/src/main.py
@@ -13,22 +13,9 @@ def create_sub_differences(line):
     
     return sub_differences
 
-def history_forwards(line):
+def next_in_sequence(line):
     sub_differences = create_sub_differences(line)
     return sum([diff[-1] for diff in sub_differences])
-
-def history_backwards(line):
-    sub_differences = create_sub_differences(line)
-    history_sum = 0
-    sign = 1
-    for diff in sub_differences:
-        history_sum += diff[0] * sign
-        sign = sign * -1
-    return history_sum
-
-assert history_backwards([10, 13, 16, 21, 30, 45]) == 5
-assert history_backwards([1, 3, 6, 10, 15, 21]) == 0
-
 
 parent_dir = Path(os.path.dirname(__file__)).parent
 abs_file_path = os.path.join(parent_dir, 'data/input.txt')
@@ -36,8 +23,9 @@ abs_file_path = os.path.join(parent_dir, 'data/input.txt')
 with open(abs_file_path) as file:
     lines = [[int(nbr) for nbr in line.strip().split()] for line in file.readlines()]
     
-    ans1 = sum([history_forwards(line) for line in lines])
+    ans1 = sum([next_in_sequence(line) for line in lines])
     print(f'Part 1: {ans1}')
 
-    ans2 = sum([history_backwards(line) for line in lines])
-    print(f'Part 1: {ans2}')
+    # reverse the sequence to get the first
+    ans2 = sum([next_in_sequence(line[::-1]) for line in lines])
+    print(f'Part 2: {ans2}')
